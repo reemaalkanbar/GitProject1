1ccb49099916fdff7e74b21fe10ec957e610fa14
reemaalkanbar <140739114+reemaalkanbar@users.noreply.github.com>
2023-07-29 13:08:52 -0400

    Update DemoDay2.java

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -5,7 +5,7 @@ public class DemoDay2 {
 	public static void main(String[] args) {
 
 		System.out.println("Test ... ");
-		
+	}
 		//add method
 		public static void add(int a , int b ) {
 			System.out.println(a+b);
@@ -29,4 +29,3 @@ public class DemoDay2 {
 			System.out.println(a-b-c);
 		}
 	}
-}
